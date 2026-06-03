b7c5aa07da1eb30065e965862b940d58a54d8271
Colib <rossinesleny@gmail.com>
2026-06-03 13:08:21 +0000

    Exercice3 fini

--- src/main/java/fr/univ_amu/iut/jdbc/DataAccessException.java
+++ src/main/java/fr/univ_amu/iut/jdbc/DataAccessException.java
@@ -13,7 +13,7 @@ import java.sql.SQLException;
  */
 public class DataAccessException extends RuntimeException {
 
-  public DataAccessException(String message, SQLException cause) {
+  public DataAccessException(String message, Throwable cause) {
     super(message, cause);
   }
 }
